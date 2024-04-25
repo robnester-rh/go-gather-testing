369e7876269f807a5bdd6b019e683da8b6d80572
Rob Nester <rnester@redhat.com>
2024-04-25 10:25:11 -0400

    Fix additional typos

--- metadata/metadata.go
+++ metadata/metadata.go
@@ -1,4 +1,4 @@
-// Copyright The enterprise Contract Contributors
+// Copyright The Enterprise Contract Contributors
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
